caa59e7fe2e111f27903c37f5f12313da5f86e58
x.karimjonov <khtkarimzhonov@gmail.com>
2024-01-17 18:15:26 +0500

    fixes

--- app/oauth/admin.py
+++ app/oauth/admin.py
@@ -1,9 +1,36 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as _UserAdmin
-
+from django.utils.translation import gettext_lazy as _
 from .models import User
 
 
 @admin.register(User)
 class UserAdmin(_UserAdmin):
-    pass
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "email", "first_name", "last_name", "is_staff")
+    ordering = ("email",)
