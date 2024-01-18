5949a0dcc2ebe2409502385f2f27e9e0700491c5
x.karimjonov <khtkarimzhonov@gmail.com>
2024-01-18 10:15:50 +0500

    add create-legal

--- app/organization/admin.py
+++ app/organization/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Organization, UserToOrganization
+
+
+class UserToOrganizationInline(admin.TabularInline):
+    extra = 0
+    model = UserToOrganization
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'inn']
+    search_fields = ['name', 'inn']
+    inlines = [UserToOrganizationInline]
