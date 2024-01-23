f6151bceeabee4c2a15c02901d5b6013c0e97ca3
x.karimjonov <khtkarimzhonov@gmail.com>
2024-01-23 15:37:54 +0500

    fixes

--- app/core/settings.py
+++ app/core/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware'
+    'django.middleware.locale.LocaleMiddleware',
     'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
