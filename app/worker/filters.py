c64417639e5079fde90ae481f48a44ddc6ff6c1c
x.karimjonov <khtkarimzhonov@gmail.com>
2025-08-05 08:38:14 +0500

    add filter has_direction

--- app/worker/filters.py
+++ app/worker/filters.py
@@ -2,12 +2,14 @@ import django_filters.rest_framework as filters
 from django.contrib.auth import get_user_model
 from django.db.models import Exists, OuterRef, When, Case, Value
 from django.utils import timezone
+from mprofid.models import WorkerInvoice
 from .models import WorkerDoc, DocType, SOON_EXPIRE_LIMIT, DOC_STATUS_EXPIRED, DOC_STATUS_NORM, DOC_STATUS_SOON_EXPIRED, DOC_STATUS
 
 
 class WorkerFilter(filters.FilterSet):
     org = filters.CharFilter("usertoorganization__org_id")
     status_doc = filters.ChoiceFilter(choices=DOC_STATUS, method="filter_status_doc")
+    has_direction = filters.BooleanFilter(method='filter_has_direction')
 
     class Meta:
         model = get_user_model()
@@ -30,6 +32,13 @@ class WorkerFilter(filters.FilterSet):
                 default=Value(DOC_STATUS_NORM)
             )
         ).filter(status_doc=value)
+
+    def filter_has_direction(self, queryset, name, value):
+        return queryset.annotate(
+            has_direction=Exists(
+                WorkerInvoice.objects.filter(worker_id=OuterRef("id"))
+            )
+        ).filter(has_direction=value)
     
 
 class DocTypeFilter(filters.FilterSet):
