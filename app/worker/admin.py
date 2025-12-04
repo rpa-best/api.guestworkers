79615571b046b58ad8f3d45dc4487d2630c85616
x.karimjonov <khtkarimzhonov@gmail.com>
2025-12-04 08:31:06 +0500

    change get_workers

--- app/worker/admin.py
+++ app/worker/admin.py
@@ -1,14 +1,5 @@
 from django.contrib import admin
-from django_celery_beat.models import IntervalSchedule, SolarSchedule, ClockedSchedule
-from django_celery_results.models import GroupResult
 from .models import DocType
-
-
-admin.site.unregister(IntervalSchedule)
-admin.site.unregister(SolarSchedule)
-admin.site.unregister(ClockedSchedule)
-
-admin.site.unregister(GroupResult)
 
 @admin.register(DocType)
 class DocTypeAdmin(admin.ModelAdmin):
