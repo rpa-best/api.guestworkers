480a19452a1482212e79b8d9963d683ed12aa09c
x.karimjonov <khtkarimzhonov@gmail.com>
2025-12-04 08:27:57 +0500

    change get_workers

--- app/worker/ftp.py
+++ app/worker/ftp.py
@@ -1,22 +1,7 @@
-import os, io
 import pandas as pd
-from ftplib import FTP, Error
-
-HOST = os.getenv("FTP_HOST")
-USER = os.getenv("FTP_HOST_USER")
-PASSWORD = os.getenv("FTP_HOST_PASSWORD")
+from django.conf import settings
 
 
 def get_workers():
-    with FTP(HOST, USER, PASSWORD) as ftp:
-        ftp.cwd('/var/www/ftp/')
-        # file_name = sorted(filter(lambda x: str(x).endswith('.csv'), ftp.nlst()), key=lambda x: ftp.voidcmd(f"MDTM {x}"))[-1]
-        file_name = 'Личный кабинет бекэнд.csv'
-        with io.BytesIO() as file:
-            try:
-                ftp.retrbinary(f'RETR {file_name}', file.write)
-            except Error:
-                file_name = sorted(filter(lambda x: str(x).endswith('.csv'), ftp.nlst()), key=lambda x: ftp.voidcmd(f"MDTM {x}"))[-1]
-                ftp.retrbinary(f'RETR {file_name}', file.write)
-            df = pd.read_csv(io.StringIO(file.getvalue().decode()), sep=';', encoding='utf8')
-            return df.astype(object).where(pd.notnull(df), None).to_dict(orient="records")
+    df = pd.read_csv(settings.BASE_DIR / 'ftp/sotrudniki.csv', sep=';', encoding='utf8')
+    return df.astype(object).where(pd.notnull(df), None).to_dict(orient="records") 
